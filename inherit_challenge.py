221b2c36c089a8862e1b3208543f4dd76e7274f1
jackie-may <jackielynnmay@gmail.com>
2021-01-22 15:30:21 -0700

    Inherit challenge
    in this file i create a parent class and two child class instances that inherit the parent class properties

---
+++ inherit_challenge.py
@@ -0,0 +1,15 @@
+
+
+class person:
+    name = 'no name provided'
+    email = ' '
+    password = 'password'
+    account_number = 0
+
+class teacher:
+    annual_pay = 60,000
+    years_experience = '5'
+
+class student:
+    gpa = 3.7
+    grade_level = '5th grade'
