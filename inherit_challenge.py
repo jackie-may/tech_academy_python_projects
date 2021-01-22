987eb4fc604f761e64cc904d93148ce30b98fc40
jackie-may <jackielynnmay@gmail.com>
2021-01-22 15:35:05 -0700

    update to inherit challenge
    added notes

--- inherit_challenge.py
+++ inherit_challenge.py
@@ -1,15 +1,20 @@
 
-
+#parent class
 class person:
     name = 'no name provided'
     email = ' '
     password = 'password'
     account_number = 0
 
-class teacher:
-    annual_pay = 60,000
-    years_experience = '5'
 
+#child class instance 1
+#the child classes will inherit the above parent class attributes
+class teacher:
+annual_pay = 60,000
+years_experience = '5'
+
+
+#child class instance 2
 class student:
     gpa = 3.7
     grade_level = '5th grade'
